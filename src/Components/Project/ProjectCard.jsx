4beb0631d4df3d55b65fe5d81dd150f023211335
Manoj Belbase <manojbelbase56@gmail.com>
2024-12-26 18:49:13 +0545

    fixing projects styles

--- src/Components/Project/ProjectCard.jsx
+++ src/Components/Project/ProjectCard.jsx
@@ -1,39 +1,55 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { FaEyeSlash, FaGithub, FaEye } from "react-icons/fa";
-import ImagePopup from "./ImagePopup"; // Import the ImagePopup component
+import ImagePopup from "./ImagePopup";
 import { motion } from "framer-motion";
-const ProjectCard = ({ item }) => {
-  const [isPopupVisible, setIsPopupVisible] = useState(false); // Control popup visibility
-  const location = useLocation();
-  const hideContent = "/projects";
+import { FaLink, FaLinkSlash } from "react-icons/fa6";
 
-  // Function to toggle the visibility of the popup
+const ProjectCard = ({ item }) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [isImageHovered, setIsImageHovered] = useState(false);
+  const location = useLocation();
+  const isHideContentPath = location.pathname.startsWith("/projects");
+
   const togglePopup = () => {
     setIsPopupVisible(!isPopupVisible);
   };
 
+  const liveButtonClasses = item?.url
+    ? "text-white"
+    : "text-accent cursor-not-allowed";
+
   return (
-    <motion.div className="border rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300">
-      {/* Project Image */}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.3 }}
+      className="border rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300"
+    >
       <div
-        onClick={togglePopup} // Toggle popup visibility when clicking on the image
-        className="h-44 sm:h-48 w-full cursor-pointer"
+        onClick={togglePopup}
+        className="h-44 sm:h-48 w-full cursor-pointer relative flex items-center justify-center"
       >
         <img
           src={item.images[0]}
           alt={item.title}
-          className="h-full w-full object-contain"
-          title="Click to view screenshots"
+          className={`h-full w-full object-contain hover:opacity-50`}
+          onMouseEnter={() => setIsImageHovered(true)}
+          onMouseLeave={() => setIsImageHovered(false)}
         />
+        {isImageHovered && (
+          <FaEye
+            className="absolute text-5xl hover:opacity-100"
+            style={{ pointerEvents: "none" }}
+          />
+        )}
       </div>
 
-      {/* Popup Component */}
       {isPopupVisible && (
         <ImagePopup images={item.images} onClose={togglePopup} />
       )}
 
-      {/* Project Details */}
       <div className="p-3 sm:p-4 flex flex-col gap-2 sm:gap-3">
         <h3 className="text-base sm:text-lg font-semibold text-white">
           {item.title}
@@ -43,18 +59,17 @@ const ProjectCard = ({ item }) => {
         </p>
         <p
           className={`text-xs sm:text-sm text-accent ${
-            location.pathname !== hideContent ? "line-clamp-2" : ""
+            !isHideContentPath ? "line-clamp-2" : ""
           }`}
         >
           {item.description}
         </p>
-
-        {/* GitHub and Live Buttons */}
         <div className="flex items-center gap-2">
           <Link
-            to={item.githubRepo}
+            to={item.githubRepo || "#"}
             target="_blank"
             rel="noopener noreferrer"
+            aria-label="GitHub repository"
             className="flex items-center gap-1 sm:gap-2 text-center text-white border-secondary border py-1 px-3 sm:px-4 transition-colors duration-200 rounded-md text-xs sm:text-sm"
           >
             <span>Github</span>
@@ -64,15 +79,13 @@ const ProjectCard = ({ item }) => {
           <Link
             to={item?.url || "#"}
             target={item?.url ? "_blank" : "_self"}
-            className={`${
-              item?.url ? "text-white" : "text-accent cursor-not-allowed"
-            } flex gap-2 sm:gap-3 items-center text-center border-secondary border py-1 px-3 sm:px-4 rounded-md transition-colors duration-200 text-xs sm:text-sm`}
+            className={`${liveButtonClasses} flex gap-2 sm:gap-3 items-center text-center border-secondary border py-1 px-3 sm:px-4 rounded-md transition-colors duration-200 text-xs sm:text-sm`}
           >
             Live
             {item?.url ? (
-              <FaEye className="text-lg sm:text-xl" />
+              <FaLink className="text-sm sm:text-xl" />
             ) : (
-              <FaEyeSlash className="text-lg sm:text-xl" />
+              <FaLinkSlash className="text-lg sm:text-xl" />
             )}
           </Link>
         </div>
