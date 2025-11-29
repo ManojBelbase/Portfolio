d728fe885b0ea24e5a57f9f4ad8948678e7f9934
manojbelbase <manojbelbase56@gmail.com>
2025-11-29 17:43:21 +0545

    ✨ feat(home): implement home page
    - add video cover
    - add hero, about me and project component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import Hero from "../Components/Hero/Hero";
 import AboutMe from "../Components/About/AboutMe";
 import ProjectContainer from "../Components/Project/ProjectContainer";
